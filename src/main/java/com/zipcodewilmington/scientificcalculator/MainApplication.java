519f0f3f57cef0edcc3a336bee1cbe48fcae5ffc
james <jamesstrine@gmail.com>
2026-03-01 12:23:52 -0500

    Have a basic calc that works with common equations but only allows 2 numbers

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,17 +1,74 @@
 package com.zipcodewilmington.scientificcalculator;
 
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+import utils.Prompt;
+
 /**
  * Created by leon on 2/9/18.
  */
-public class MainApplication {
-    public static void main(String[] args) {
-        Console.println("Welcome to my calculator!");
-        String s = Console.getStringInput("Enter a string");
-        Integer i = Console.getIntegerInput("Enter an integer");
-        Double d = Console.getDoubleInput("Enter a double.");
 
-        Console.println("The user input %s as a string", s);
-        Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a d", d);
+
+public class MainApplication {
+    
+    public static int calculate(int a, int b, String operator) {
+        int result = 5;
+        switch (operator) {
+            case "+":
+                result = Console.add(a, b);
+                break;
+            case "-":
+                result = Console.subtract(a, b);
+                break;
+            case "*":
+                result = Console.multiply(a, b);
+                break;
+            case "/":
+                result = Console.divide(a, b);
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+        return result;
+    }
+
+    
+    //     Console.println("");
+    //     String s = Console.getStringInput("Enter a string");
+    //     Integer i = Console.getIntegerInput("Enter an integer");
+    //     Double d = Console.getDoubleInput("Enter a double");
+    // }
+    
+    
+    // public static void main(String[] args, Object userInput, Object prompt, String operator) {
+        
+    //     if (userInput instanceof String) {
+    //         System.out.println();
+    //         String userInputStr = Console.getStringInput(prompt);
+    //         Console.println(userInputStr);
+    //     } else if (userInput instanceof Integer) {
+    //         System.out.println();
+    //         Integer userInputInt = Console.getIntegerInput(prompt);
+    //         Console.println(userInputInt);
+    //     } else if (userInput instanceof Double) {
+    //         System.out.println();
+    //         Double userInputDbl = Console.getDoubleInput(prompt);
+    //         Console.println(userInputDbl);
+    //     }
+    //     else {
+    //         System.out.println("Invalid input type");
+    //     }
+    //     System.out.println(prompt);
+    // }
+
+    public static void main(String[] args) {
+        String prompt = "Enter an operator (+, -, *, /): ";
+        String operatorInput = Console.getStringInput(prompt);
+        Integer num1Input = Console.getIntegerInput("Enter the first number: ");
+        Integer num2Input = Console.getIntegerInput("Enter the second number: ");
+        int result = calculate(num1Input, num2Input, operatorInput);
+        Console.println("Result: %d", result);
+        
     }
 }
--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -3,5 +3,37 @@ package com.zipcodewilmington.scientific_calculator;
 /**
  * Created by leon on 2/9/18.
  */
+
+class Prompt {
+    private String prompt;
+    private String userInput;
+
+    public Prompt(String prompt, String userInput) {
+        this.prompt = prompt;
+        this.userInput = userInput;
+    }
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public String getUserInput() {
+        return userInput;
+    }
+}
 public class TestMainApplication {
+
+
+    public static int add(int a, int b) {
+        int sum = a + b;
+        System.out.println(sum);
+        return sum;
+    }
+
+    public static int multiply(int a, int b) {
+        int product = a * b;
+        System.out.println(product);
+        return product;
+    }
+
 }
